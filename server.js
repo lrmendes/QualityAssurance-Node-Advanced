7c46ba798b44b9671bf48ec5e363a3082e0a26e8
lrmendes <lrmen14@gmail.com>
2021-01-04 22:31:57 +0000

    ADD -> Solved Exercises 2 & 3 - Setup Passport and Session

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-//const pug = require('pug');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -13,8 +14,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug')
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.route('/').get((req, res) => {
-  res.render('pug/index.pug');
+  res.render('pug/index.pug', {title: 'Hello', message: 'Please login'});
 });
 
 app.listen(process.env.PORT || 3000, () => {
